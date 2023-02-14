16e1d5bf961af2cc8b82725f2423bba3fbb0f450
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-02-14 23:20:31 +0700

    Revert "[Project - MAIN] Update (2)"
    This reverts commit 10749d968b142ea6ad30eb769bcd5bfaa38d938a.

--- src/utils/HandleTimer.js
+++ src/utils/HandleTimer.js
@@ -1,39 +1,32 @@
-function getBackTime(time) {
-  const today = Date.now();
-  const inputTime = new Date(time).getTime();
-  const outTime = 1000 * 60 * 60 * 11; // mySQL time
-  return today - inputTime - outTime;
+const getBackTime = (time) => {
+    const today = Date.now();
+    const inputTime = new Date(time).getTime();   
+    return today - inputTime;
 }
 
-function getStringBackTime(time) {
-  const seconds = getBackTime(time) / 1000;
+const getStringBackTime = (time) => {
+    const seconds = getBackTime(time) / 1000;
 
-  const minutes = Math.floor(seconds / 60),
-    hours = Math.floor(seconds / (60 * 60)),
-    days = Math.floor(seconds / (24 * 60 * 60)),
-    months = Math.floor(seconds / (30 * 24 * 60 * 60))
-
-  if (months > 0) {
-    return months + " tháng trước";
-  }
-  if (days > 0) {
-    return days + " ngày trước";
-  }
-  if (hours > 0) {
-    return hours + " giờ trước";
-  }
-  if (minutes > 0) {
-    return minutes + " phút trước";
-  }
-  return seconds.toFixed(0) + "giây trước";
-}
-
-function formatDateTime(datetimeString) {
-  let datetimeFormat = new Date(datetimeString);
-  return datetimeFormat.toLocaleString("vi");
+    const   minutes = Math.floor(seconds / 60),
+            hours = Math.floor(seconds / (60 * 60)),
+            days = Math.floor(seconds / (24 * 60 * 60)),
+            months = Math.floor(seconds / (30 * 24 * 60 * 60))
+    
+    if (months > 0) {
+        return months + " tháng trước";
+    }
+    if (days > 0) {
+        return days + " ngày trước";
+    }
+    if (hours > 0) {
+        return hours + " giờ trước";
+    }
+    if (minutes > 0) {
+        return minutes + " phút trước";
+    }
+    return seconds + "giây trước";
 }
 
 export {
-  getStringBackTime,
-  formatDateTime,
+    getStringBackTime,
 }
