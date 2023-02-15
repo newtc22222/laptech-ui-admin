5a0064dcab81b9ee51911b40563abb27a024db7e
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-02-15 21:46:15 +0700

    [Pages - Product] create Product Table and index

--- src/pages/Product/ProductTable.jsx
+++ src/pages/Product/ProductTable.jsx
@@ -1,7 +1,74 @@
 import React from 'react';
+import Loading from '../../components/common/Loading';
+import useFetch from '../../hooks/useFetch';
+import useTable from '../../hooks/useTable';
+import { getCurrencyString } from '../../utils/HandleCurency';
 
-const ProductTable = () => {
-  return <div>ProductTable</div>;
-};
+const titleButtonUpdate = 'Cập nhật';
+const titleButtonDelete = 'Xóa';
+const currencyUnit = ' đồng';
+
+function ProductTable({
+  productList,
+  handleSetUpdateMode,
+  handleShowDeleteModal
+}) {
+  const { data: brandList } = useFetch('/brands');
+  const { data: categoryList } = useFetch('/categories');
+
+  if (brandList === null || categoryList === null) {
+    return <Loading />;
+  }
+
+  return useTable(
+    [
+      'Tên sản phẩm',
+      'Thương hiệu',
+      'Danh mục',
+      'Ảnh đại diện',
+      'Ngày ra mắt',
+      'Số lượng trong kho',
+      'Giá niêm yết',
+      'Thiết lập'
+    ],
+    () =>
+      productList?.map(product => (
+        <tr key={product.id} className="text-center">
+          <td className="fw-bolder">{product.name}</td>
+          <td className="text-secondary">{brandList[product.brandId].name}</td>
+          <td className="text-secondary">
+            {categoryList[product.categoryId].name}
+          </td>
+          <td>
+            <img src={''} />
+          </td>
+          <td>{product.releasedDate}</td>
+          <td>
+            <p className="fw-bold">0</p>
+          </td>
+          <td>
+            <p className="fw-bold">
+              {getCurrencyString(product.listedPrice) + currencyUnit}
+            </p>
+          </td>
+          <td style={{ width: '10%' }}>
+            <button
+              className="btn btn-secondary w-100 mb-2"
+              onClick={() => handleSetUpdateMode(product)}
+            >
+              {titleButtonUpdate}
+            </button>{' '}
+            <br />
+            <button
+              className="btn btn-danger w-100"
+              onClick={() => handleShowDeleteModal(product.id, product.name)}
+            >
+              {titleButtonDelete}
+            </button>
+          </td>
+        </tr>
+      ))
+  );
+}
 
 export default ProductTable;
--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,34 +1,99 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useCallback } from 'react';
+import { useSelector } from 'react-redux';
+import useWorkspace from '../../hooks/useWorkspace';
+import WorkMode from '../../common/WorkMode';
 import apiProducts from '../../apis/product/product.api';
 
-import { getCurrencyString } from '../../utils/HandleCurency';
+import ProductTable from './ProductTable';
+import ProductForm from './ProductForm';
+
+import ModalCustom from '../../components/Modal';
+import Loading from '../../components/common/Loading';
+
+const pageName = 'Thương hiệu';
+const objectName = 'products';
+const titleButtonAdd = 'Thêm thông tin';
 
 const ProductPage = () => {
-  const dispatch = useDispatch();
+  const accessToken = useSelector(state => state.auth.accessToken);
+  const [
+    dispatch,
+    Navigate,
+    workMode,
+    showModal,
+    productEdit,
+    modalValue,
+    action
+  ] = useWorkspace();
+
+  if (accessToken === null || accessToken === undefined)
+    return <Navigate to="/auth/login" />;
+
   const { productList, isFetching, error } = useSelector(
-    state => state.products
+    state => state[objectName]
   );
 
+  // Loading
   useEffect(() => {
     apiProducts.getAllProducts(dispatch);
   }, []);
 
+  // Show delete modal
+  const handleShowDeleteModal = useCallback((productId, productName) => {
+    action.addModalValue(
+      `Xác nhận xoá thông tin ${pageName.toLowerCase()}`,
+      `Bạn có thực sự muốn loại bỏ ${pageName.toLowerCase()} ${productName} khỏi hệ thống không?`,
+      () => {
+        apiProducts.deleteProduct(dispatch, productId, accessToken);
+        action.showModal(false);
+      }
+    );
+    action.showModal(true);
+  }, []);
+
+  // Change work mode
+  if (workMode === WorkMode.create) {
+    return (
+      <ProductForm handleBack={() => action.changeWorkMode(WorkMode.view)} />
+    );
+  }
+  if (workMode === WorkMode.edit) {
+    return (
+      <ProductForm
+        product={productEdit}
+        handleBack={() => action.changeWorkMode(WorkMode.view)}
+      />
+    );
+  }
+
   if (isFetching) {
-    return <p>Loading...</p>;
+    return <Loading />;
   }
 
   return (
-    <ul>
-      {productList?.map(product => (
-        <li>
-          <p>{product.id}</p>
-          <p>{product.name}</p>
-          {/* <p>{ product.quantity }</p> */}
-          <p>{getCurrencyString(product.price) + ' vnđ'}</p>
-        </li>
-      ))}
-    </ul>
+    <div>
+      {showModal && (
+        <ModalCustom
+          show={showModal}
+          setShow={action.showModal}
+          props={modalValue}
+        />
+      )}
+      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 className="h2">{pageName}</h1>
+        <button
+          className="btn btn-primary fw-bold"
+          onClick={action.setCreateMode}
+        >
+          {titleButtonAdd}
+        </button>
+      </div>
+      <ProductTable
+        productList={productList}
+        handleSetUpdateMode={product => action.setUpdateMode(product)}
+        handleShowDeleteModal={(id, name) => handleShowDeleteModal(id, name)}
+      />
+    </div>
   );
 };
 
