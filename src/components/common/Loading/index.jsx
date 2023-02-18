4f8c9668eb5f4a6e2376608fcf57393c83a3a24e
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-02-19 00:35:18 +0700

    [Components - Loading] Update more effect

--- src/components/common/Loading/index.jsx
+++ src/components/common/Loading/index.jsx
@@ -4,9 +4,23 @@ function Loading() {
   return (
     <div className="text-center">
       <div
-        className="spinner-grow text-primary"
+        className="spinner-grow text-danger"
         role="status"
         style={{ width: '3rem', height: '3rem' }}
+      >
+        <span className="visually-hidden">Loading...</span>
+      </div>
+      <div
+        className="spinner-grow text-warning ms-3 me-3"
+        role="status"
+        style={{ width: '3rem', height: '3rem', animationDelay: '0.1s' }}
+      >
+        <span className="visually-hidden">Loading...</span>
+      </div>
+      <div
+        className="spinner-grow text-success"
+        role="status"
+        style={{ width: '3rem', height: '3rem', animationDelay: '0.2s' }}
       >
         <span className="visually-hidden">Loading...</span>
       </div>
