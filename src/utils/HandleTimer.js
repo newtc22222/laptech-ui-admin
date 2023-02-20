6ff7aec6c0f8d086d6336630cd9d5223639fd048
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-02-20 07:46:35 +0700

    [Project - Util] Clear code

--- src/utils/HandleTimer.js
+++ src/utils/HandleTimer.js
@@ -1,8 +1,8 @@
 function getBackTime(time) {
   const today = Date.now();
   const inputTime = new Date(time).getTime();
-  const outTime = 1000 * 60 * 60 * 11; // mySQL time
-  return today - inputTime - outTime;
+  // const outTime = 1000 * 60 * 60 * 11; // mySQL time
+  return today - inputTime;
 }
 
 function getStringBackTime(time) {
@@ -11,29 +11,26 @@ function getStringBackTime(time) {
   const minutes = Math.floor(seconds / 60),
     hours = Math.floor(seconds / (60 * 60)),
     days = Math.floor(seconds / (24 * 60 * 60)),
-    months = Math.floor(seconds / (30 * 24 * 60 * 60))
+    months = Math.floor(seconds / (30 * 24 * 60 * 60));
 
   if (months > 0) {
-    return months + " tháng trước";
+    return months + ' tháng trước';
   }
   if (days > 0) {
-    return days + " ngày trước";
+    return days + ' ngày trước';
   }
   if (hours > 0) {
-    return hours + " giờ trước";
+    return hours + ' giờ trước';
   }
   if (minutes > 0) {
-    return minutes + " phút trước";
+    return minutes + ' phút trước';
   }
-  return seconds.toFixed(0) + "giây trước";
+  return seconds.toFixed(0) + 'giây trước';
 }
 
 function formatDateTime(datetimeString) {
   let datetimeFormat = new Date(datetimeString);
-  return datetimeFormat.toLocaleString("vi");
+  return datetimeFormat.toLocaleString('vi');
 }
 
-export {
-  getStringBackTime,
-  formatDateTime,
-}
+export { getStringBackTime, formatDateTime };
