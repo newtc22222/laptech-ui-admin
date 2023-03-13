74f112e9d339da5bdf8a169d78ab516ab355103f
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-03-13 23:34:35 +0700

    [component - TextEditor] base on React-quill

---
+++ src/components/common/TextEditor/index.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, false] }],
+    [{ color: [] }, { background: [] }],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote', 'code-block'],
+    [
+      { list: 'ordered' },
+      { list: 'bullet' },
+      { indent: '-1' },
+      { indent: '+1' }
+    ],
+    ['link', 'image'],
+    ['clean']
+  ]
+};
+
+const formats = [
+  'header',
+  'color',
+  'background',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'blockquote',
+  'code-block',
+  'list',
+  'bullet',
+  'indent',
+  'link',
+  'image'
+];
+
+const TextEditor = ({ tab, handleSaveContent }) => {
+  const [value, setValue] = useState(tab.content || '');
+
+  return (
+    <>
+      <ReactQuill
+        theme="snow"
+        value={value}
+        onChange={setValue}
+        modules={modules}
+        formats={formats}
+      />
+      <div className="d-flex flex-row-reverse mt-2 gap-2">
+        <button
+          className="btn btn-outline-secondary"
+          onClick={() => setValue(tab.content)}
+        >
+          Revert
+        </button>
+        <button
+          className="btn btn-outline-primary"
+          onClick={() => handleSaveContent(value, tab.id)}
+        >
+          Save
+        </button>
+      </div>
+      {/* <div dangerouslySetInnerHTML={{ __html: value }}></div> */}
+    </>
+  );
+};
+
+export default TextEditor;
