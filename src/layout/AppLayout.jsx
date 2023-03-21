741e2bb941eb9cd49eb54475a2c35cb70127c74d
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-03-22 06:51:13 +0700

    [Layout - App] restructure with react-toastify

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,35 +1,21 @@
 import React from 'react';
 import { Outlet } from 'react-router';
-import { useSelector } from 'react-redux';
-import { ToastContainer } from 'react-bootstrap';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
-import ToastCustom from '../components/common/ToastCustom';
+import { ToastContainer } from 'react-toastify';
 
 /**
  * @since 2022-12-22
  */
 function AppLayout() {
-  const toastList = useSelector(state => state.toastList);
-
   return (
     <>
       <Header />
+      <ToastContainer />
       <div className="container-fluid">
         <div className="row">
-          <Sidebar />
-          <main className="col-auto col-md-9 col-lg-10 ms-sm-auto px-md-4">
-            {toastList.length > 0 && (
-              <ToastContainer
-                containerPosition="fixed"
-                position="top-end"
-                className="p-3"
-              >
-                {toastList.map(props => {
-                  return <ToastCustom key={props.id} props={props} />;
-                })}
-              </ToastContainer>
-            )}
+          <Sidebar sidebarClass={'col-auto col-lg-2 px-0'} />
+          <main className="col-auto col-sm-10 col-lg-10 container-md">
             <Outlet />
           </main>
         </div>
