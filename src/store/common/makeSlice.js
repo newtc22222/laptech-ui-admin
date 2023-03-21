88082138f8bfb26e55adf6aa4edcb468eda22e2b
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-03-22 06:56:59 +0700

    [helper] build closure function

---
+++ src/store/common/makeSlice.js
@@ -0,0 +1,54 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+/**
+ * @param {string} objectName
+ */
+function makeSlice(objectName) {
+  const initialState = {
+    data: null,
+    isFetching: false,
+    error: false
+  };
+
+  return createSlice({
+    name: objectName,
+    initialState,
+    reducers: {
+      fetchStart: state => {
+        state.isFetching = true;
+      },
+      fetchFailed: state => {
+        state.isFetching = false;
+        state.error = true;
+      },
+      getSuccess: (state, action) => {
+        state.isFetching = false;
+        state.error = false;
+        state.data = action.payload;
+      },
+      createSuccess: (state, action) => {
+        state.isFetching = false;
+        state.error = false;
+        state.data = [...state.data, action.payload];
+      },
+      updateSuccess: (state, action) => {
+        console.log(action.payload);
+        state.isFetching = false;
+        state.error = false;
+        state.data = state.data.map(object => {
+          if (object.id === action.payload.id) {
+            return { ...object, ...action.payload };
+          }
+          return object;
+        });
+      },
+      deleteSuccess: (state, action) => {
+        state.isFetching = false;
+        state.error = false;
+        state.data = state.data.filter(object => object.id !== action.payload);
+      }
+    }
+  });
+}
+
+export default makeSlice;
