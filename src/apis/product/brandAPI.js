2a78d6317fe7b93aec074a16fe168e37b0c5ecf1
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-03-22 23:00:35 +0700

    [Project - main] Remove useless source

--- src/apis/product/brandAPI.js
+++
@@ -1,6 +0,0 @@
-import makeCallAPI from '../helper/makeCallAPI';
-import { action } from '../../store/slice/brand.slice';
-
-const apiBrand = makeCallAPI('brands', action);
-
-export default apiBrand;
