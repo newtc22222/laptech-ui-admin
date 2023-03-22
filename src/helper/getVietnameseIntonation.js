aed0bf94715606be860d9af76098ad99455311f7
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-03-22 23:05:21 +0700

    [helper - translate] eng -> vi

--- src/helper/getVietnameseIntonation.js
+++ src/helper/getVietnameseIntonation.js
@@ -1,6 +1,13 @@
 const subText = {
-  brands: 'Thương hiệu',
-  categories: 'Phân loại'
+  brands: 'thương hiệu',
+  categories: 'phân loại',
+  discounts: 'mã chiết khấu',
+  labels: 'nhãn thuộc tính',
+  imports: 'đơn nhập hàng',
+  invoices: 'đơn bán hàng',
+  products: 'sản phẩm',
+  roles: 'phân quyền',
+  users: 'người dùng'
 };
 
 export default subText;
