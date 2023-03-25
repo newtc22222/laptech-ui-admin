2142a3d53785bbe89c9e81d901e897f0c9f22985
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-03-25 18:14:57 +0700

    [Project - main] Delete old files

--- src/pages/Product/components/ImageBox.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ImageBox = () => {
-  return <div>ImageBox</div>;
-};
-
-export default ImageBox;
--- src/store/slice/import.slice.js
+++
@@ -1,6 +0,0 @@
-import makeSlice from '../helpers/makeSlice';
-
-const importSlice = makeSlice('import');
-
-export const action = importSlice.actions;
-export default importSlice.reducer;
