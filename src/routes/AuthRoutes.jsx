cd4e886d82c075f1432f6ab84b0c2218408951f8
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-06-06 11:54:18 +0700

    [component - AuthRoute] Update jwt -> refreshtoken

--- src/routes/AuthRoutes.jsx
+++ src/routes/AuthRoutes.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useLocation, Navigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import jwtDecode from 'jwt-decode';
 
 import AppLayout from '../components/layout/AppLayout';
+import { authService } from '../services';
 import { createLocalStorage } from '../utils/createStorage';
 
 function AuthRoutes() {
   const location = useLocation();
+  const dispatch = useDispatch();
 
   const storage = createLocalStorage('laptech');
   const user = storage.get('user');
@@ -22,13 +25,12 @@ function AuthRoutes() {
     return <Navigate to="/auth/login" state={{ from: location }} replace />;
   }
 
-  // const { exp } = jwtDecode(storage.get('accessToken'));
-  // if (exp <= Date.now() / 1000) {
-  //   console.log('here');
-  //   authService.refreshToken(dispatch, {
-  //     refreshToken: storage.get('refreshToken')
-  //   });
-  // }
+  const { exp } = jwtDecode(storage.get('accessToken'));
+  if (exp <= Date.now() / 1000) {
+    authService.refreshToken(dispatch, {
+      refreshToken: storage.get('refreshToken')
+    });
+  }
 
   return <AppLayout />;
 }
