d8a9440664962954d3c82db85a7994498122a748
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-06-10 15:16:21 +0700

    [page - Login] remake with react-hook-form

--- src/pages/ProductExperiences/content.js
+++ src/pages/ProductExperiences/content.js
@@ -1,5 +1,36 @@
 const content = {
-  pageName: 'Trải nghiệm sản phẩm'
+  pageName: 'Trải nghiệm sản phẩm',
+  selectProduct: 'Chọn sản phẩm muốn xem',
+  feedback: 'Đánh giá sản phẩm',
+  comment: 'Bình luận về sản phẩm',
+  card: {
+    viewUser: 'Thông tin người dùng',
+    viewProduct: 'Thông tin sản phẩm',
+    disableFeedback: 'Ẩn đánh giá',
+    replyComment: 'Phản hồi bình luận',
+    disableComment: 'Ẩn bình luận'
+  },
+  form: {
+    replyComment: 'Phản hồi bình luận',
+    errorMessage: 'Vui lòng nhập nội dung phản hồi!',
+    errorMessageForMin: 'Vui lòng nhập nội dung có độ dài tối thiểu là x!'
+  },
+  view: {
+    product: {},
+    user: {
+      id: 'Mã người dùng: ',
+      phone: 'Số điện thoại: ',
+      email: 'Email: ',
+      gender: 'Giới tính: ',
+      dateOfBirth: 'Sinh nhật: ',
+      accountStatus: 'Trạng thái tài khoản: ',
+      genderSub: {
+        MALE: 'Nam',
+        FEMALE: 'Nữ',
+        OTHER: 'Khác'
+      }
+    }
+  }
 };
 
 export default content;
